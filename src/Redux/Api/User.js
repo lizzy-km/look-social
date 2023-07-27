826385edb544219c9201108f849b2401cfd4892e
Fuck the Error! <74085442+Kaung-Myat-Soe@users.noreply.github.com>
2023-07-27 21:56:31 +0630

    vercel.json

--- src/Redux/Api/User.js
+++ src/Redux/Api/User.js
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
  const AccessKey = 'aMikd9b0_tQ9n2Ie63a2Z5CUut6Bj0cHwJ_4uTqcHTI';
  const keyPixel= '1QPzLcqtWlGk33n4Lgj6qKKi80NroyP5fNlUnH5JSDVDgW9mYUzdeswI'
 
+//  EABktHGmTmAYBO0jzZA57Tt6kReQNSdZA7mWJWH59y3JoIvvd4PIqIErfgLKuxUsP1xMSZBdRg90IwoEtlBbsB9UDsV4zetBhTFFParli5sKk6Xx2t8gzkrYI0OSM1qkzz4nWRFouh7cgWGAs6jjhQDpifTGn8p3jguwjht6LkJ7TNtoTZAd9cDJntvttVhUfZBotxwDXl
 
  const token = 'v-3kbeaGhKxDTu3c4UXDYIKn_bKhCZ12poYXuCJMqaw'
 export const UserApi = createApi({
